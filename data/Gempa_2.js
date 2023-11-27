68a59d85c179541ffef522704039a4f33df5d5d4
gempadunia <152171918+gempadunia@users.noreply.github.com>
2023-11-27 18:11:27 +0700

    Add files via upload

---
+++ data/Gempa_2.js
@@ -0,0 +1,14 @@
+var json_Gempa_2 = {"type":"FeatureCollection","name":"Gempa_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[
+	{"type":"Feature","properties":{
+		"ID":"hgd66",
+		"MagDepth":"M8.7 - 20 km",
+		"Waktu":"Tahun 1390",
+		"Link":"<a href=\"event.html\">Info Lengkap</a>"},"geometry":{"type":"MultiPoint","coordinates":
+		[[99.7,-2.12]]}},
+
+
+
+
+
+
+		]}
