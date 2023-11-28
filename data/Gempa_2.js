154d330f69706a730b779b64cf32a7f93b0d2271
gempadunia <152171918+gempadunia@users.noreply.github.com>
2023-11-28 17:16:06 +0700

    Update Gempa_2.js

--- data/Gempa_2.js
+++ data/Gempa_2.js
@@ -56,6 +56,62 @@ var json_Gempa_2 = {"type":"FeatureCollection","name":"Gempa_2","crs":{"type":"n
 		"Waktu":"1916-09-09 19:48:00 WIB",
 		"Link":"<a href=\"hgd81.html\">Info Lengkap</a>"},"geometry":{"type":"MultiPoint","coordinates":
 		[[109.12,-7.62]]}},
+{"type":"Feature","properties":{
+		"ID":"hgd75",
+		"MagDepth":"M8.6 - 25 km",
+		"Waktu":"Tahun 1703",
+		"Link":"<a href=\"hgd75.html\">Info Lengkap</a>"},"geometry":{"type":"MultiPoint","coordinates":
+		[[100.61,-3.34]]}},
+{"type":"Feature","properties":{
+		"ID":"hgd77",
+		"MagDepth":"M5.8 - 10 km",
+		"Waktu":"1924-11-12 13:45:00 WIB",
+		"Link":"<a href=\"hgd77.html\">Info Lengkap</a>"},"geometry":{"type":"MultiPoint","coordinates":
+		[[109.87,-7.33]]}},
+{"type":"Feature","properties":{
+		"ID":"hgd78",
+		"MagDepth":"M5.5 - 10 km",
+		"Waktu":"1924-12-02 08:21:00 WIB",
+		"Link":"<a href=\"hgd78.html\">Info Lengkap</a>"},"geometry":{"type":"MultiPoint","coordinates":
+		[[109.91,-7.29]]}},
+{"type":"Feature","properties":{
+		"ID":"hgd57",
+		"MagDepth":"M6.0 - 10 km",
+		"Waktu":"1852-11-26 06:45:00 WIB",
+		"Link":"<a href=\"hgd57.html\">Info Lengkap</a>"},"geometry":{"type":"MultiPoint","coordinates":
+		[[113.01,-7.68]]}},
+{"type":"Feature","properties":{
+		"ID":"hgd64",
+		"MagDepth":"M7.9 - 10 km",
+		"Waktu":"Tahun 1314",
+		"Link":"<a href=\"hgd64.html\">Info Lengkap</a>"},"geometry":{"type":"MultiPoint","coordinates":
+		[[99.75,-3.14]]}},
+{"type":"Feature","properties":{
+		"ID":"hgd15",
+		"MagDepth":"M9.0 - 35 km",
+		"Waktu":"Abad ke-10",
+		"Link":"<a href=\"hgd15.html\">Info Lengkap</a>"},"geometry":{"type":"MultiPoint","coordinates":
+		[[96.24,3.11]]}},
+{"type":"Feature","properties":{
+		"ID":"hgd56",
+		"MagDepth":"M7.1 - 150 km",
+		"Waktu":"1852-12-20 07:39:00 WIB",
+		"Link":"<a href=\"hgd56.html\">Info Lengkap</a>"},"geometry":{"type":"MultiPoint","coordinates":
+		[[106.5,-6.16]]}},
+{"type":"Feature","properties":{
+		"ID":"hgd59",
+		"MagDepth":"M5.8 - 10 km",
+		"Waktu":"1931-01-21 07:04:14 WIB",
+		"Link":"<a href=\"hgd59.html\">Info Lengkap</a>"},"geometry":{"type":"MultiPoint","coordinates":
+		[[109.1,-7.26]]}},
+{"type":"Feature","properties":{
+		"ID":"hgd55",
+		"MagDepth":"M7.4 - 10 km",
+		"Waktu":"1847-11-16 10:18:00 WIB",
+		"Link":"<a href=\"hgd55.html\">Info Lengkap</a>"},"geometry":{"type":"MultiPoint","coordinates":
+		[[108.33,-6.38]]}},
+
+
 
 
 
